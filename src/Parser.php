f26ad3d77ed8782172e7069b0f64259a78a44d73
Leontev Daniil <daniil.leontev@rtmis.ru>
2023-08-10 17:15:10 +0500

    add parser

--- src/Parser.php
+++ src/Parser.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace Differ\Parser;
+
+use Symfony\Component\Yaml\Yaml;
+
+function getParseCode(string $dataFile, string $extension): mixed
+{
+    switch ($extension) {
+        case 'json':
+            return json_decode($dataFile, true);
+        case 'yml':
+        case 'yaml':
+            return Yaml::parse($dataFile);
+        default:
+            throw new \Exception('Unknown extension ' . $extension);
+    }
+}
