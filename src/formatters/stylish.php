09a4cb7c77a394836dff1aba041959dd7811c24d
daniil <75467751+daniileontev@users.noreply.github.com>
2023-08-16 17:35:02 +0500

    refactoring

--- src/formatters/stylish.php
+++ src/formatters/stylish.php
@@ -9,33 +9,53 @@ function toString(mixed $value): string
     }
     return trim(var_export($value, true), "'");
 }
-function stringify(array $arr, int $depth = 0): string
+
+
+function getStylish(mixed $value, string $replacer = ' ', int $spaceCount = 4): string
 {
-    $indent = str_repeat(" ", $depth * 4);
-    $bracketIndent = str_repeat(' ', $depth * 4 + 4);
-    $result = "";
-    foreach ($arr as $key => $value) {
-        if (is_array($value)) {
-            $result .= $indent . formatKey($key) . ": {\n";
-            $result .= stringify($value, $depth + 1);
-            $result .= $bracketIndent . "}\n";
-        } elseif ($value === '') {
-            $result .= $indent . formatKey($key) . ":" . "\n";
-        } else {
-            $result .= $indent . formatKey($key) . ": " . toString($value) . "\n";
-        }
+    if (!is_array($value)) {
+        return toString($value);
     }
 
-    return $result;
-}
-function formatKey(string $key): string
-{
-    if (str_starts_with($key, '  + ')) {
-        return $key;
-    } elseif (str_starts_with($key, '  - ')) {
-        return $key;
-    } elseif (str_starts_with($key, '    ')) {
-        return $key;
-    }
-    return '    ' . $key;
+    $iter = function ($currentValue, $depth) use (&$iter, $replacer, $spaceCount) {
+
+        if (!is_array($currentValue)) {
+            return toString($currentValue);
+        }
+
+        $indentLength = $spaceCount * $depth;
+        $shiftToLeft = 2;
+        $indentForUnchanged = str_repeat($replacer, $indentLength);
+        $indentForChanged = str_repeat($replacer, $indentLength - $shiftToLeft);
+        $bracketIndent = str_repeat($replacer, $indentLength - $spaceCount);
+
+        $line = array_map(
+            function ($item, $key) use ($iter, $indentForChanged, $indentForUnchanged, $depth) {
+                if (!is_array($item)) {
+                    return $indentForUnchanged . $key . ": " . $iter($item, $depth + 1);
+                }
+                if (!array_key_exists('type', $item)) {
+                    return $indentForUnchanged . $key . ": " . $iter($item, $depth + 1);
+                }
+                if ($item['type'] === "added") {
+                    return $indentForChanged . "+ " . $item[$key] . ": " . $iter($item['value2'], $depth + 1);
+                }
+                if ($item['type'] === "removed") {
+                    return $indentForChanged . "- " . $item[$key] . ": " . $iter($item['value1'], $depth + 1);
+                }
+                if ($item['type'] === "updated") {
+                    return $indentForChanged . "- " . $item[$key] . ": " . $iter($item['value1'], $depth + 1)
+                        . "\n" . $indentForChanged . "+ " . $item[$key] . ": " . $iter($item['value2'], $depth + 1);
+                }
+                return $indentForUnchanged . $key . ": " . $iter($item['value1'], $depth + 1);
+            },
+            $currentValue,
+            array_keys($currentValue)
+        );
+
+        $result = ['{', ...$line, $bracketIndent . '}'];
+
+        return implode("\n", $result);
+    };
+    return $iter($value, 1);
 }
