28eed85fdba2d73d3cf74c1a59f702ca8302988f
daniil <75467751+daniileontev@users.noreply.github.com>
2023-08-17 16:05:46 +0500

    refactor unit tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -16,17 +16,16 @@ final class DifferTest extends TestCase
 
     public function testGenDiffJson()
     {
-        $expected = file_get_contents($this->getFixtureFullPath("differJson.txt"));
+        $expected = file_get_contents($this->getFixtureFullPath("json.txt"));
         $this->assertEquals($expected, genDiff(
             $this->getFixtureFullPath("file1.json"),
-            $this->getFixtureFullPath("file2.json"),
-            'stylish'
+            $this->getFixtureFullPath("file2.json")
         ));
     }
 
     public function testGenDiffYaml()
     {
-        $expected = file_get_contents($this->getFixtureFullPath("differJson.txt"));
+        $expected = file_get_contents($this->getFixtureFullPath("json.txt"));
         $this->assertEquals($expected, genDiff(
             $this->getFixtureFullPath("yamlFile1.yaml"),
             $this->getFixtureFullPath("ymlFile2.yml"),
