01ea91d9ccab8716ee9056770fa798373f755488
daniil <75467751+daniileontev@users.noreply.github.com>
2023-08-17 16:27:19 +0500

    add json format

--- src/Formatter.php
+++ src/Formatter.php
@@ -4,12 +4,14 @@ namespace Differ\Formatters;
 
 use function Differ\Formatters\Stylish\getStylish;
 use function Differ\Formatters\Plain\getPlain;
+use function Differ\Formatters\Json\getJson;
 
 function getFormat(mixed $diff, string $format): string
 {
     return match ($format) {
         "stylish" => getStylish($diff),
         "plain" => getPlain($diff),
+        "json" => getJson($diff),
         default => throw new \Exception("Unknown format - " . $format),
     };
 }
