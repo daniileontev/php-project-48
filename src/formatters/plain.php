b3e52f2eebf0e11fa257da115e70aa8e7bd000bf
daniil <75467751+daniileontev@users.noreply.github.com>
2023-08-17 16:04:28 +0500

    add plain.php

--- src/formatters/plain.php
+++ src/formatters/plain.php
@@ -2,7 +2,40 @@
 
 namespace Differ\Formatters\Plain;
 
-function getPlain()
+use function Differ\Parser\getNormalValue;
+
+function getPlain($diff, $nextKey = ""): string
 {
-    return "ОГО ВАУ РАБОТАЕТ";
+    $newProperties = array_map(function ($node) use ($nextKey) {
+        $key = $node['key'];
+        $type = $node['type'];
+        $value1 = $node['value1'];
+        $value2 = $node['value2'];
+
+        if ($nextKey === "") {
+            $newKey = $key;
+        } else {
+            $newKey = $nextKey . "." . $key;
+        }
+
+        switch ($type) {
+            case "nested":
+                return getPlain($value1, $newKey);
+            case "removed":
+                return "Property '$newKey' was removed";
+            case "added":
+                $normalValue = getNormalValue($value2);
+                return "Property '$newKey' was added with value: " . $normalValue;
+            case "updated":
+                $normalValue1 = getNormalValue($value1);
+                $normalValue2 = getNormalValue($value2);
+                return "Property '$newKey' was updated. From $normalValue1 to $normalValue2";
+            case "unchanged":
+                break;
+            default:
+                throw new \Exception("Unknown type - " . $type);
+        }
+    }, $diff);
+
+    return implode("\n", array_filter($newProperties));
 }
